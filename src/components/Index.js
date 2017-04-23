2bab665ee897b0e41ce6ecf418fdde79bd905537
robdel12 <robertdeluca19@gmail.com>
2017-04-23 15:07:51 -0500

    Initial Commit 🎉

---
+++ src/components/Index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Index(props) {
+  return (
+    <div>Hey</div>
+  );
+}
+
+export default Index;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Index from './components/Index';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import 'tachyons';
+import './index.css';
+
+// Paste your endpoint for the Simple API here.
+// Info: https://github.com/graphcool-examples/react-apollo-instagram-example#2-create-graphql-api-with-graphcool
+const networkInterface = createNetworkInterface({ uri: '__SIMPLE_API_ENDPOINT__' });
+
+const client = new ApolloClient({networkInterface});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Route exact path='/' component={Index} />
+      </div>
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
